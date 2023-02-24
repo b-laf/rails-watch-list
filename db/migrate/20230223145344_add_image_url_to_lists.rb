aad2ddb662236c95f1f2f6974625c0b43660a0f8
Bastien Lafont <lafont.bastien@gmail.com>
2023-02-24 15:52:03 +0100

    end of front before hero

--- db/migrate/20230223145344_add_image_url_to_lists.rb
+++
@@ -1,5 +0,0 @@
-class AddImageUrlToLists < ActiveRecord::Migration[7.0]
-  def change
-    add_column :lists, :imgurl, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -56,7 +56,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_24_114024) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "imgurl"
   end
 
   create_table "movies", force: :cascade do |t|
